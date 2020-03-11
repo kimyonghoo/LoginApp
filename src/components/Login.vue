91f892181777d9540ba574f170fbfc9bb84773a9
김용후 <yhkim13@cyberlogitec.com>
2020-03-11 13:23:43 +0900

    renewal login page

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -11,24 +11,26 @@
               </v-toolbar>
               <v-card-text>
                 <v-form>
-                  <v-text-field v-model="email" label="email" prepend-icon="person" :rules="emailRules" autocomplete="email" @focus="activeLogin"/>
-                  <v-text-field v-model="password" label="Password" prepend-icon="lock" type="password" :rules="required" autocomplete="current-password" @focus="activeLogin"/>
+                  <v-text-field v-model="email" label="email" prepend-icon="person" :rules="emailRules" autocomplete="email" v-on:keyup.enter="login"/>
+                  <v-text-field v-model="password" label="Password" prepend-icon="lock" type="password" :rules="required" autocomplete="current-password" v-on:keyup.enter="login"/>
                 </v-form>
               </v-card-text>
               <v-card-actions>
                 <v-spacer />
-                 <v-switch 
-                  v-model="click" 
-                  :loading="loading"
-                  :label="label"
-                  :disabled="disabled"
-                  :error="error"
-                  @change="login()"
-              ></v-switch>
+                <v-btn @click="login">LOGIN</v-btn>
               </v-card-actions>
             </v-card>
           </v-col>
         </v-row>
+        <template>
+          <div class="text-center ma-2">
+            <v-snackbar
+              v-model="snackBar"
+              :color="snackColor"
+            >{{ snackMessage }}              
+            </v-snackbar>
+          </div>
+        </template>
       </v-container>
     </v-content>
   </v-app>
@@ -40,12 +42,6 @@ export default {
     return {
       email:'',
       password: '',
-      message:'',
-      click:false,
-      label:"click to login",
-      disabled:false,
-      loading:false,
-      error:false,
       emailRules: [
         v => !!v || 'E-mail is required',
         v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
@@ -53,36 +49,29 @@ export default {
       required: [
         value => !!value || "Password is required."
       ],
+      snackBar:false,
+      snackMessage:'',
+      snackColor: '',
     };
   },
   methods: {
-    isProgressing: function(flag){
-      this.disabled=flag;
-      this.click=flag;
-      this.loading=flag;
-    },
     login: function() {
-      this.isProgressing(true);
-      this.label="Progressing...";
+      this.snackBar = true;
+      this.snackColor = 'info';
+      this.snackMessage = 'Progressing...';
       this.$http.post("/auth/signin",{email:this.email, password:this.password}).then(response => {
         if(response.data.status === true){
+          this.snackColor = 'success';
+        this.snackMessage = 'Success';
           localStorage.setItem("token",response.data.token);
-          router.push({ name: "home", params:{id:response.data.user} }, () => {});
+          router.push({ name: "home", params:{id:response.data.user}}, () => {});
         }else{
-          this.isProgressing(false);
-          this.error=true;
-          this.label=response.data.message?response.data.message:"login failed";
+          this.snackBar = true;
+          this.snackColor = 'error';
+          this.snackMessage = response.data.message?response.data.message:"login failed";
         }
      });
     },
-    activeLogin : function(){
-      this.isProgressing(false);
-      this.label="click to login";
-      this.error=false;
-    },
-    isNull: function(obj){
-      if(obj==="") this.disabled===true
-    }
   }
 };
 </script>
